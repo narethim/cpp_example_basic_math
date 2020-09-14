b6d1fce2d511d424fd0384ae706ed8cd29031c41
Nareth Im <narethi@yahoo.com>
2020-09-14 16:57:34 -0700

    Initial commit

---
+++ src/basic_math.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : basic_math.cpp
+// Author      : Example
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "CBasicMath.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	CBasicMath basicMath;
+    int x = 4;
+    int y = 5;
+
+    int z1 = basicMath.Addition(x, y);
+    printf("\nAddition Result: %d\n", z1);
+
+    int z2 = basicMath.Multiply(x, y);
+    printf("\nAddition Result: %d\n", z2);
+
+	return 0;
+}
